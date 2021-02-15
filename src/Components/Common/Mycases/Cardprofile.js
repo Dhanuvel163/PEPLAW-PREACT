1184317725d37c4c231d493367311134d4f6851c
Dhanuvel163 <dhanuram99@gmail.com>
2021-02-15 13:57:24 +0530

    HOVER CARD OPTI

--- src/Components/Common/Mycases/Cardprofile.js
+++ src/Components/Common/Mycases/Cardprofile.js
@@ -26,7 +26,7 @@ export default function Cardprofile(props){
             <div className="card bg-danger card-style four-box-shadow" style={{marginTop:50}} >
                 <div className="card-header">
                     <div onMouseEnter={toggleIn} onMouseLeave={toggleOut}>
-                        <Link to={`/profile/${props.casedata.User._id}`} style={{color:'white'}}>
+                        <Link to={`/profile/${props.casedata.User[0]._id}`} style={{color:'white'}}>
                         <svg width="20" height="18" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                         <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
